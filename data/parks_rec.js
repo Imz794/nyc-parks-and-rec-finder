dadd49ccc51d3501a0cd467834fce87d4d419c6e
tommyv34 <tommyv34@yahoo.com>
2025-12-14 18:04:38 -0500

    Bug fixes and added hours logic for rec centers

--- data/parks_rec.js
+++ data/parks_rec.js
@@ -27,3 +27,50 @@ export const allList = async (page) => {
 
     return merged.slice(skip, skip + PAGE_SIZE);
 };
+
+export const hasHours = (facility) => {
+    let hours = facility.hours;
+    for(let day of Object.values(hours)){
+        if(day.startTime != null){
+            return true;
+        }
+    }
+    return false;
+};
+
+export const getHours = (facility) => {
+    let hours = facility.hours;
+    let newHours = hours;
+    for(let day of Object.values(newHours)){
+        if(day.startTime == null){
+            day.startTime = "Closed";
+            day.endTime = "";
+        }
+        else{
+            let tfhour = Number(day.startTime.split(':')[0]);
+            let minute = day.startTime.split(':')[1];
+            let ap = 'AM';
+            if(tfhour >= 12){
+                ap = 'PM';
+                tfhour = tfhour - 12;
+            }
+            if(tfhour == 0){
+                tfhour = 12;
+            }
+            day.startTime = `${tfhour}:${minute}${ap}`;
+
+            let tfhoure = Number(day.endTime.split(':')[0]);
+            let minutee = day.endTime.split(':')[1];
+            let ape = 'AM';
+            if(tfhoure >= 12){
+                ape = 'PM';
+                tfhoure = tfhoure - 12;
+            }
+            if(tfhoure == 0){
+                tfhoure = 12;
+            }
+            day.endTime = `${tfhoure}:${minutee}${ape}`;
+        }
+    }
+    return newHours;
+}
--- routes/list_routes.js
+++ routes/list_routes.js
@@ -1,14 +1,12 @@
 import {Router} from 'express';
 const router = Router();
-import { parkList, recList } from '../data/parks_rec.js';
-import { getFacilityById, hasReviewed } from '../data/facilities.js';
-import { parks, rec_centers } from '../config/mongoCollections.js';
-import { ObjectId } from 'mongodb';
+import { getHours, hasHours, parkList, recList } from '../data/parks_rec.js';
+import { getFacilityById } from '../data/facilities.js';
 
 router.route('/rec_centers/:page').get(async (req, res) => {
     let page = parseInt(req.params.page, 10);
     if (isNaN(page) || page < 0) {
-        return res.status(400).render('error', { error: 'Invalid page number' });
+        return res.status(400).render('error', { error: 'Invalid page number', user: req.session.user });
     }
 
     let back = page > 0 ? page - 1 : 0;
@@ -26,7 +24,7 @@ router.route('/rec_centers').get(async (req, res) => {
 router.route('/parks/:page').get(async (req, res) => {
     let page = parseInt(req.params.page, 10);
     if (isNaN(page) || page < 0) {
-        return res.status(400).render('error', { error: 'Invalid page number' });
+        return res.status(400).render('error', { error: 'Invalid page number', user: req.session.user });
     }
 
     let back = page > 0 ? page - 1 : 0;
@@ -39,6 +37,24 @@ router.route('/parks/:page').get(async (req, res) => {
 
 router.route('/parks').get(async (req, res) => {
     res.redirect('/parks/0');
+});
+
+router.route('/rec_centers/:id/hours').get(async (req, res) => {
+    let facilId = req.params.id;
+    let facility = await getFacilityById(facilId);
+    let hasH = false;
+    let hours = {};
+
+    if(!facility){
+        return res.status(404).render('error', { error: 'Facility not found', user: req.session.user });
+    }
+
+    if(hasHours(facility)){
+        hasH = true;
+        hours = getHours(facility);
+    }
+
+    res.render('hours', { facility: facility, hasHours: hasH, hours: hours, user: req.session.user });
 });
 
 export default router;
